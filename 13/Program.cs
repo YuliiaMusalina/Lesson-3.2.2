8ab46ad4f318ed2cdfcbe040a075495a299e6dbf
Ulia Kamalova <mysula@mail.ru>
2022-08-16 17:22:43 +0500

    13 задача

---
+++ 13/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99)
+{
+   if(number < 1000)
+   {
+    int lastdDigit = number % 10;
+    Console.WriteLine(lastdDigit);
+   }
+   else 
+   {
+    Console.WriteLine("Третьей цифры нет");
+   }
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
